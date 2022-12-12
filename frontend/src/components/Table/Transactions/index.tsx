528f24a742a44a3bf64e8c5df7af6b166740a7f2
zelleous <manalozhuntelle@gmail.com>
2022-12-13 00:30:30 +0800

    feat/remove icon

--- frontend/src/components/Table/Transactions/index.tsx
+++ frontend/src/components/Table/Transactions/index.tsx
@@ -340,9 +340,7 @@ const TransactionTable = () => {
 												className={`card p-0 px-1 py-1 ${getStatusColor(`${transaction.status}`)}`}
 												onClick={() => setID(transaction.id)}
 											>
-												{transaction.status}<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-													<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-												</svg>
+												{transaction.status}
 											</label>
 
 											{role === "Brgy. Admin" && id === transaction.id ? (
@@ -370,8 +368,7 @@ const TransactionTable = () => {
 									<label htmlFor={transaction.id + "Pending"} className="modal cursor-pointer">
 										<label className="modal-box relative" htmlFor="">
 											<h3 className="text-lg ">Are you sure you want to update?</h3>
-											<div className="modal-action">
-												<label
+											<div className="modal-action">												<label
 													className="btn btn-primary"
 													onClick={() => {
 														formController.updateTransaction({
